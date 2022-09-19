fcfde2f6a188ad330a880d1d886eac554eb3dbb9
amiconi <binateblj@gmail.com>
2022-09-19 20:59:54 +0000

    creation du workshop de dom

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//console.log(document.getElementsByTagName("h1")[0]); retourne un tableau
+
+//console.log(document.getElementsByClassName('title'));retourne un tableau
+
+//console.log(document.getElementById('title'))retourne un element
+
+//console.logdocument.querySelector('h1'));retourne un seul Element
+
+//console.log(document.querySelector('.title')); il retourne le premier ayant la classe title
+
+//console.log(document.querySelector('#title')); retourne le premier ayant id title
+
+//console.log(document.querySelectorAll('.title')); retourne un tableau NodeList
+
+//console.log(document.querySelector('.title').innerHTML); retourn un tableau Nodelist
+
+//getAttribute && setAtribute
+
+//console.log (document.querySelector('#title').getAttribute('class'));
+
+//document.querySelector('#title').setAttribute('class,text')
